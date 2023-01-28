85122b6622cf9eca6df41947583a8c3fcd772307
dazzel3 <aakk9350@naver.com>
2023-01-28 16:04:56 +0900

    chore: 타입스크립트 및 alias 설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,20 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+
+const path = require("path")
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
