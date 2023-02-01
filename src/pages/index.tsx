614c07068cb23269eaa870b9b8381c8ec1415077
dazzel3 <aakk9350@naver.com>
2023-02-02 01:40:21 +0900

    refactor: css import 위치 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../assets/styles/font.css';
 import GlobalStyle from 'components/common/GlobalStyle';
 import Header from 'components/header/Header';
 import CategoryList from 'components/main/CategoryList';
