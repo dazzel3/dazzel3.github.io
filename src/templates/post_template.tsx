a2475a2ec57f6b6c7b407433b93f89c4a62dc119
dazzel3 <aakk9350@naver.com>
2023-02-04 21:26:04 +0900

    feat: 포스트 template에 Query 추가

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,3 +1,4 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
 interface PostTemplateProps {}
@@ -9,3 +10,26 @@ const PostTemplate = props => {
 };
 
 export default PostTemplate;
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
