c589f3975c8b3d0f93c8a7e3586da561946eb448
dazzel3 <aakk9350@naver.com>
2023-02-12 16:31:46 +0900

    chore: gatsby-plugin-react-helmet 설치 및 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
     },
     `gatsby-plugin-emotion`,
     'gatsby-plugin-sitemap',
+    `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
