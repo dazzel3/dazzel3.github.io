75a4d9f1209fdae6c69cbdfabcdc0db74df95bdf
dazzel3 <aakk9350@naver.com>
2023-05-28 21:11:27 +0900

    chore: 카테고리별 포스트 수 추가

--- src/components/main/CategoryList.tsx
+++ src/components/main/CategoryList.tsx
@@ -24,7 +24,7 @@ const CategoryList = ({
               active={name === selectedCategory}
               key={name}
             >
-              {name}
+              {name} ({count})
             </LinkButton>
           </CategoryItem>
         ))}
