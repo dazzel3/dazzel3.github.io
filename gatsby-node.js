cdbda878e64a97ecfea661c492cc4557084199e4
dazzel3 <aakk9350@naver.com>
2024-02-18 23:39:06 +0900

    feat: add context path

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,7 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
         components: path.resolve(__dirname, 'src/components'),
         utils: path.resolve(__dirname, 'src/utils'),
         hooks: path.resolve(__dirname, 'src/hooks'),
+        context: path.resolve(__dirname, 'src/context'),
       },
     },
   });
