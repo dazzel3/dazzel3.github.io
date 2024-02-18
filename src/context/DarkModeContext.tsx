bdca1c622a9e02e754089126618e7dc2c61f24f7
dazzel3 <aakk9350@naver.com>
2024-02-18 23:40:24 +0900

    feat: create DarkModeContext

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, createContext, useContext, useState } from 'react';
+
+export interface ContextType {
+  darkMode: boolean;
+  toggleMode: () => void;
+}
+interface Props {
+  children: ReactNode;
+}
+
+const DarkModeContext = createContext<ContextType>({
+  darkMode: false,
+  toggleMode: () => {
+    return null;
+  },
+});
+
+export const DarkModeProvider: React.FC<Props> = ({ children }) => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const toggleMode = () => {
+    setDarkMode(!darkMode);
+    console.log('toggleMode');
+  };
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => useContext(DarkModeContext);
